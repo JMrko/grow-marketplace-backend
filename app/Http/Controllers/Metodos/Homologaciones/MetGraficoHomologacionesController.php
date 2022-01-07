f897068f6ebe6aa745796a913441628466377dfb
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-01-07 10:58:02 -0500

    obtener data de competencia

--- app/Http/Controllers/Metodos/Homologaciones/MetGraficoHomologacionesController.php
+++ app/Http/Controllers/Metodos/Homologaciones/MetGraficoHomologacionesController.php
@@ -15,13 +15,58 @@ class MetGraficoHomologacionesController extends Controller
         $mensaje = '';
         $paginas = [];
         $fechas_pagina = [];
+
+        $labels_fechas = [];
+
+        $data_grafico = [];
+        $arr_data_grafico = array(
+            "label" => "SOFTYS",
+            "data"  => [],
+        );
+
+        // {
+        //     label: 'Dataset 1',
+        //     data: [1,2,3,4,50,6,7],
+            
+        // },
+
         //OBTENER LAS FECHAS PARA EL EJE X
         $fechas = dtpdatospaginas::join('fecfechas as fec', 'fec.fecid', 'dtpdatospaginas.fecid')
                                     ->where('proid', $proid)
-                                    ->distinct()
+                                    ->distinct('fecid') // Agregar
                                     ->get([
+                                        'fecid',
                                         'fecfecha'
                                     ]);//orderby de fecfechas
+
+        foreach($fechas as $fecha){
+            $labels_fechas[] = $fecha->fecfecha;
+        }
+
+        // BUCLE PARA RETORNAR LA DATA DEL PRODUCTO ORIGINAL 
+
+        foreach ($fechas as $key => $fecha) {
+            
+            $prp = prppreciosproductos::where('proid', $proid)
+                                        ->where('fecid', $fecha->fecid)
+                                        ->first([
+                                            'prpprecio'
+                                        ]);
+
+            if($prp){
+                $data_grafico[] = $prp->prpprecio;
+            }else{
+                $data_grafico[] = "0";
+            }
+
+        }
+
+        $arr_data_grafico = array(
+            "label" => "SOFTYS",
+            "data"  => $data_grafico,
+        );
+
+
                                    
         //OBTENER LOS ID PAGINAS DE LAS FECHAS OBTENIDAS
         foreach ($fechas as $fecha) {
@@ -71,5 +116,14 @@ class MetGraficoHomologacionesController extends Controller
             'fechas'    => $fechas,
             'datos'     => $fechas_pagina
         ]);
+    }
+
+    public function MetObtenerProductosCompetenciaGrafico(Request $request)
+    {
+
+        $re_fechas = $request['fechas'];
+        $re_pagid  = $request['pagid'];
+        $re_proid  = $request['proid'];
+
     }
 }
