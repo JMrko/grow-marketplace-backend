de2e8a990146c8575d26a102c6da8d85d167bfff
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-03-09 01:34:16 -0500

    bnueva version del excel

--- app/Http/Controllers/Metodos/SalvacionController.php
+++ app/Http/Controllers/Metodos/SalvacionController.php
@@ -11,7 +11,7 @@ class SalvacionController extends Controller
     {
 
         return response()->json([
-            'link' => "https://pricing-backend.softys-leadcorporate.com/Excels/MarketPlaces-FM07032022XA.xlsx"
+            'link' => "https://pricing-backend.softys-leadcorporate.com/Excels/MarketPlaces-FM09032022XA.xlsx"
         ]);
 
     }
